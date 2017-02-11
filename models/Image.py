429ee82b62f1748138351b2803e6d69c200f87a3
Jacob Bieker <jacob@bieker.tech>
2017-02-11 15:41:55 +0100

    Add updating some Image values from JSON response
    Currently, on the ones that are available in the initial response, those that require follow up requests (sizes, owner, etc.) are currently left out

--- models/Image.py
+++ models/Image.py
@@ -33,12 +33,41 @@ class Image(object):
         self.point_of_interest = {}
         self.point_of_interest_crops = {}
         self.regions = {}
+        self.image_format = ""
 
         self.smugmug = smugmug
 
 
         # Use to hold JSON response for image, possibly, as a cache?
         self.json = None
+
+    def _unpack_json(self, json_dictionary):
+        """
+        Unpack to dictionary containing the JSON response for the image.
+        Used to update values for Image object.
+        ":param json_dictionary: Dictionary containing the JSON of the "Response" key
+        :return:
+        """
+        image_root = json_dictionary["Image"]
+        self.title = image_root["Title"]
+        self.caption = image_root["Caption"]
+        self.watermarked = image_root["Watermarked"]
+        self.latitude = image_root["Latitude"]
+        self.longitude = image_root["Longitude"]
+        self.can_edit = image_root["CanEdit"]
+        self.hidden = image_root["Hidden"]
+        self.collectable = image_root["Collectable"]
+        self.altitude = image_root["Altitude"]
+        self.image_key = image_root["ImageKey"]
+        self.is_video = image_root["IsVideo"]
+        self.original_height = image_root["OriginalHeight"]
+        self.original_width = image_root["OriginalWidth"]
+        self.original_size = image_root["OriginalSize"]
+        self.keywords = image_root["KeywordArray"]
+        self.image_format = image_root["Format"]
+
+        self.uri = json_dictionary["Uri"]
+        return 0
 
     def get_size(self, size="Original"):
         """ Convenience function to return the URL for the requested size"""
